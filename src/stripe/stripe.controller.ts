f5022333aa666c440969de1d9de6eb80df56b5b2
RodrLucas <lucasrodriguessantos0@gmail.com>
2025-03-22 21:48:48 -0300

    stripe controller

--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -1,21 +1,55 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  Controller,
+  Post,
+  Body,
+  Headers,
+  Req,
+  RawBodyRequest,
+} from '@nestjs/common';
 import { StripeService } from './stripe.service';
-import { SubscriptionGuard } from '../common/guards/subscription.guard';
-import { RequiresPlan } from '../common/decorators/requires-plan.decorator';
-
+import { SupabaseService } from '../supabase/supabase.service';
 @Controller('stripe')
 export class StripeController {
-  constructor(private stripeService: StripeService) {}
+  constructor(
+    private stripeService: StripeService,
+    private supabaseService: SupabaseService,
+  ) {}
 
-  @Get('products')
-  async getProducts() {
-    return await this.stripeService.getProducts();
+  // @Get('plans')
+  // async getPlans(@Headers('x-region') region: string = 'BR') {
+  //   return await this.stripeService.getAllPlans(region);
+  // }
+
+  @Post('create-checkout')
+  async createCheckout(
+    @Body() body: { userId: string; email: string; priceId: string },
+  ) {
+    const { userId, email, priceId } = body;
+
+    const url = await this.stripeService.createCheckoutSession(
+      userId,
+      email,
+      priceId,
+    );
+    return { url };
   }
 
-  @Get('customers')
-  @UseGuards(SubscriptionGuard)
-  @RequiresPlan('premium') // Este é apenas um exemplo, defina os nomes dos planos conforme seu negócio
-  async getCustomers() {
-    return await this.stripeService.getCustomers();
+  @Post('create-portal')
+  async createPortal(@Body() body: { userId: string }) {
+    const { userId } = body;
+
+    const url = await this.stripeService.createPortalSession(userId);
+    return { url };
+  }
+
+  @Post('webhook')
+  async handleWebhook(
+    @Headers('stripe-signature') signature: string,
+    @Req() req: RawBodyRequest<Request>,
+  ) {
+    await this.stripeService.handleWebhook(signature, req.rawBody as Buffer);
+    return { received: true };
   }
 }
